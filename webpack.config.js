f101b708f5aa2943f4435e46f3f3e0d2ad148dee
Haroen Viaene <hello@haroen.me>
2018-05-29 23:41:07 +0200

    move to /public

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/index.bs.js',
@@ -6,5 +7,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [new CopyWebpackPlugin([{ from: 'public' }])],
   devtool: 'source-map',
 };
